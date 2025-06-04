6e52f996f031a79f21f648ca10aba15db983c834
V1ct0rhugo22 <victorhero53@gmail.com>
2025-06-04 14:26:16 -0300

    Back

---
+++ hackaton/src/main/java/com/senai/hackaton/Repository/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.senai.hackaton.Repository;
+
+import com.senai.hackaton.Entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+
+
+}
